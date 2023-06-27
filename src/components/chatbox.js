23fd7f464f2ead9753623535a012795e45795ffa
JamshedK <karimnazarovj@gmail.com>
2023-06-26 20:01:01 -0500

    more styling changes

--- src/components/chatbox.js
+++ src/components/chatbox.js
@@ -4,10 +4,15 @@ import MsgEntry from './msg_entry'
 import Prompt from './prompt';
 import Response from './response';
 
+import user_profile from '../assets/chatbox/user_profile.svg'
+import ai_profile from '../assets/chatbox/ai_profile.svg'
+
 function ChatBox (){
     const [prompt, setPrompt] = useState('')
     const [isLoading, setIsLoading] = useState(true);
 
+    const bgObj = {"user": "bg-[#3c586e]",
+                    "ai": "bg-[#2f4454]"}
     const getAPIResponse = async (prompt) => {  
         console.log('Waiting for the server to respond')
         try {
@@ -60,7 +65,7 @@ function ChatBox (){
         messageComponent = messagesArray.map((message, i)=>{
         return(
             <div>
-                <Prompt key={"prompt-" + i}text={message.user_message}/> 
+                <Prompt key={"prompt-" + i} text={message.user_message} /> 
                 <Response key={"response-" + i} text={message.gpt_reply} isLoading={isLoading}/>
             </div>
         );
@@ -74,8 +79,9 @@ function ChatBox (){
         <div className="bg-[#2f4454] flex w-full h-full flex-col " >         
             {/* <div>{messageComponent}</div> */}
             <div>
-                <Prompt key={"prompt-" + 2}text='Hello'/> 
-                <Response key={"response-" + 2} text='Hi, I am your AI' isLoading={isLoading}/>
+                <Prompt key={"prompt-" + 2} profile_image={user_profile}  bgColor="bg-[#3c586e]" text="Hello there! I hope you're doing well today. How can I be of assistance to you? Whether you have a specific question, need information on a particular topic, or simply want to engage in a conversation, I'm here to help. Feel free to share your thoughts or ask anything you'd like, and I'll provide you with a detailed and informative response."/> 
+                <Prompt key={'prompt-'+2} profile_image={ai_profile} text="Hi, I'm your AI" bgColor = {bgObj.ai}/>
+                <Prompt key={'prompt-'+2} profile_image={user_profile} text="How are you?" bgColor = {bgObj.user}/>
             </div>
             <div className='mx-25 absolute bottom-0 left-1/2 mb-8'>
                 <MsgEntry setMessagesArray={setMessagesArray} messagesArray={messagesArray} setPrompt={setPrompt}/>
--- src/components/prompt.js
+++ src/components/prompt.js
@@ -1,7 +1,29 @@
+import star_icon from '../assets/common/star_icon.svg'
+import star_filled_icon from '../assets/common/star_filled_icon.svg'
+import more_icon from '../assets/chatbox/more_icon.svg'
+import comment_icon from '../assets/chatbox/comment_icon.svg'
+
+
 const Prompt = (props) => {
     return(
-        <div className="bg-[#3c586e] h-full flex p-5 pl-14" >
-            <h1 className="text-white">{props.text}</h1>
+        <div className={"flex flex-row justify-between space-x-4 align-top p-5 pl-14 " + props.bgColor} >
+            <div className='w-full'>
+                <div className='inline-flex w-full space-x-4'>
+                    <img className='w-9 h-9' src={props.profile_image}/>
+                    <p className="text-white xl:max-w-[50%] lg:max-w-[65%] w-full leading-7">{props.text}</p>
+                </div>
+            </div>
+            <div className='flex-shrink-0 inline-flex space-x-4 pr-1 h-fit'>
+                <button>
+                    <img className='w-7' src={star_icon}/>
+                </button>
+                <button>
+                    <img className='w-6' src={comment_icon}/>
+                </button>
+                <button>
+                    <img className='w-6' src={more_icon}/>
+                </button>
+            </div>
         </div>
     );
 }
