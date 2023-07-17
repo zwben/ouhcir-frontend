e19bb274efd2ddde653630169d7f7d0922f97867
JamshedK <karimnazarovj@gmail.com>
2023-07-17 13:10:51 -0500

    Saving prompts and response in database

--- src/components/chatbox.js
+++ src/components/chatbox.js
@@ -1,20 +1,29 @@
-import {useState} from 'react';
-import React, { useRef, useEffect } from 'react';
+import {useState, useContext} from 'react';
+import { useRef, useEffect } from 'react';
 import MsgEntry from './msg_entry'
 import Prompt from './prompt';
 
 import { openai } from '../openai-config';
-
 import user_profile from '../assets/chatbox/user_profile.svg'
 import ai_profile from '../assets/chatbox/ai_profile.svg'
 import CommentPopUp from './CommentPopUp';
+import { addDoc, collection } from 'firebase/firestore';
+import TaskContext from '../context/task-context';
+import AuthContext from '../context/auth-context';
+import { db } from '../firebase-config';
+import { uid } from 'uid';
 
 function ChatBox (){
     const [showCommentPopup, setShowCommentPopup] = useState(false)
     const [prompt, setPrompt] = useState('')
     const [response, setResponse] = useState('')
+    const [promptID, setPromptID] = useState('')
+    const [responseID, setResponseID] = useState('')
     const [isLoading, setIsLoading] = useState(false);
     const [promptResponseArray, setPromptResponseArray] = useState([])
+
+    const taskCtx = useContext(TaskContext)
+    const authCtx = useContext(AuthContext)
 
     const bgObj = {"user": "bg-[#3c586e]",
                     "ai": "bg-[#2f4454]"}
@@ -23,21 +32,49 @@ function ChatBox (){
         window.scrollTo(0, document.documentElement.scrollHeight);
     }, [promptResponseArray]);
                     
-    const getAPIResponse = async (array) => {  
+    const getAPIResponse = async (array, promptID) => {  
         try {
             setIsLoading(true)
-            const completion = await openai.createChatCompletion({
+            // Filter out the IDs from the array
+            const filteredMessages = array.map((message) => {
+                const { role, content } = message;
+                return { role, content };
+            });
+          const completion = await openai.createChatCompletion({
                 model: "gpt-3.5-turbo",
-                messages: array,
+                messages: filteredMessages,
                 });
-            console.log('Actual: Getting openAI response')
-            array.push(completion.data.choices[0].message);
+            const message = completion.data.choices[0].message
+            const tempResponseID = uid()
+            setResponseID(tempResponseID)
+            array.push({...message, id: tempResponseID});
             setPromptResponseArray([...array])
+            // Save the response to Firestore database
+            await saveResponseToFirestore(message, promptID, tempResponseID);
         } catch (error) {
             console.error('Error fetching data:', error);
         }
         setIsLoading(false)
     }
+
+    const saveResponseToFirestore = async (message, promptID, tempResponseID) => {
+        try {
+            const promptRef = collection(db, 'chatsInduvidual');
+            console.log('Setting promptID is saveResponseToFirestore ' + promptID)
+            const docRef = await addDoc(promptRef, {
+                id: tempResponseID,
+                taskID: taskCtx?.taskID || '',
+                prompt: message.content,
+                responseTo: promptID,
+                userID: authCtx?.user.uid || '',
+                role: 'assistant',
+                timestamp: new Date(),
+            });
+        } catch (error) {
+            console.error("Error saving prompt:", error);
+        }
+      };
+      
 
     // Create an array of message components
     const messageComponents = promptResponseArray.map((message, index) => (
@@ -60,8 +97,6 @@ function ChatBox (){
             </div>
     ));
 
-    console.log(promptResponseArray)
-
     return(
         <div className="bg-[#2f4454] flex w-full h-full justify-center" >         
             <div className='w-full mb-36'>
@@ -70,6 +105,8 @@ function ChatBox (){
             </div>
             <div className='fixed bottom-0 mb-8 w-[50%]'>
                 <MsgEntry 
+                    setPromptID = {setPromptID}
+                    responseID = {responseID}
                     setPromptResponseArray={setPromptResponseArray} promptResponseArray={promptResponseArray}
                     setPrompt={setPrompt} getAPIResponse={getAPIResponse}/>
             </div>
--- src/components/msg_entry.js
+++ src/components/msg_entry.js
@@ -1,15 +1,18 @@
-import {useState, useRef} from 'react';
-import Prompt from './prompt';
+import {useState, useRef, useContext} from 'react';
 import send_message_icon from "../assets/msg_entry/send_message_icon.svg"
 import microphone_icon from "../assets/msg_entry/microphone_icon.svg"
-import { openai } from '../openai-config';
-
+import TaskContext from '../context/task-context';
+import AuthContext from '../context/auth-context';
+import { db } from '../firebase-config';
+import { addDoc, collection } from 'firebase/firestore';
+import { uid } from 'uid';
 
 const MsgEntry = (props) => {
      
     const [prompt, setPrompt] = useState(null);
     const textRef = useRef();  
-    
+    const taskCtx = useContext(TaskContext)
+    const authCtx = useContext(AuthContext)
     const handleTextareaChange = () => {
         if (textRef.current) {
             textRef.current.style.height = '7px'; // Reset the height 7px
@@ -20,25 +23,33 @@ const MsgEntry = (props) => {
     const sendPrompt = async (e) => {
         const newMessage = textRef.current.value
         if (newMessage.trim() != ''){
-            console.log('message: ' + newMessage);
-            props.setPrompt(newMessage)
-            const updatedMessagesArray = [...props.promptResponseArray, {role: "user", content: newMessage}];
-            // get the response from API
-            props.setPromptResponseArray(updatedMessagesArray);
-            textRef.current.value = '';
-            handleTextareaChange()
-            // get API response
-            await props.getAPIResponse(updatedMessagesArray);
-            window.scrollTo(0, document.documentElement.scrollHeight);
-
-            // For testing
-            // const completion = await openai.createChatCompletion({
-            //     model: "gpt-3.5-turbo",
-            //     messages: updatedMessagesArray,
-            //     });
-            // console.log('Getting openAI response for testing')
-            // var response = completion.data.choices[0].message;
-            // console.log(response)
+            // Save the prompt to Firestore database
+            try {
+                const promptRef = collection(db, 'chatsInduvidual');
+                const promptID = uid()
+                console.log(promptID)
+                props.setPromptID(promptID)
+                const docRef = await addDoc(promptRef, {
+                        id: promptID,
+                        taskID: taskCtx?.taskID || '',
+                        responseTo: props.responseID,
+                        prompt: newMessage,
+                        userID: authCtx?.user.uid || '', 
+                        role: 'user',
+                        timestamp: new Date(),
+                    });
+                props.setPrompt(newMessage)
+                const updatedMessagesArray = [...props.promptResponseArray, {role: "user", content: newMessage, id: promptID}];
+                // get the response from API
+                props.setPromptResponseArray(updatedMessagesArray);
+                textRef.current.value = '';
+                handleTextareaChange()
+                // get API response
+                await props.getAPIResponse(updatedMessagesArray, promptID);
+                window.scrollTo(0, document.documentElement.scrollHeight);
+            } catch (error) {
+                console.error("Error saving prompt:", error);
+            }
         }
     }
 
